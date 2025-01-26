794b1309258641feae7af3e8801aeb6a868f57a6
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-01-26 12:45:59 +0600

    Create config.go
    Signed-off-by: fortishield <161459699+FortiShield@users.noreply.github.com>

---
+++ config/config.go
@@ -0,0 +1,30 @@
+// Package config centralize all logic around the config file (config.yml by default).
+// The only available function is Load() which will load the file into dedicated structs.
+// It also performns validations.
+package config
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+	"github.com/khulnasoft/packages-registry/logger"
+)
+
+// Load will load the configuration yaml file into Configuration. It will also run several
+// checks and return an error if one validation fails.
+func Load() (*Configuration, error) {
+	var config Configuration
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	if err := validator.New().Struct(config); err != nil {
+		return nil, err
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	logger.LogInfo("Config loaded")
+	return &config, nil
+}
